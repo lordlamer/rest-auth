8074fa5d088ffd885e12dc7b5fa361498e408497
fhabermann <habermann@2im.de>
2014-04-07 09:51:50 +0200

    * create extra route for get and post with request parameters

--- lib/Auth/Application.php
+++ lib/Auth/Application.php
@@ -27,7 +27,7 @@ class Application {
 	public function run() {
 		$app = new \Slim\Slim();
 
-		// ldap auth
+		// ldap auth via GET request and username and password in url
 		$app->get('/auth/ldap/:username/:password', function ($username, $password) {
 			// load ldap adapter
 			$auth = new \Auth\Ldap($this->configFile);
@@ -51,6 +51,31 @@ class Application {
 			header('Content-type: application/json');
 			echo json_encode($res);
 		});
+
+		// ldap auth via GET and POST request
+		$app->map('/auth/ldap', function() use ($app) {
+			// load ldap adapter
+			$auth = new \Auth\Ldap($this->configFile);
+
+			// default result
+			$res = array(
+				'status' => 'ERROR',
+				'message' => ''
+			);
+
+			// check for valid credentials
+			if($auth->verify($app->request->get('username'), $app->request->get('password'))) {
+				$res = array(
+					'status' => 'SUCCESS'
+				);
+			} else {
+				$res['message'] = $auth->getMessage();
+			}
+
+			// show json result
+			header('Content-type: application/json');
+			echo json_encode($res);
+		})->via('GET', 'POST');
 
 		$app->run();
 	}
